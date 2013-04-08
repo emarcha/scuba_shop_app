e923d84164d776c76af87c1b4b6844fae9a859f7
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 17:27:53 -0500

    Update partials, css, and test cases

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,35 +2,28 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page } 
+  
   describe "Home page" do 
-    it "should have the content 'The Lion Fish Scuba Shop'" do 
-      visit '/static_pages/home'
-      expect(page).to have_content('The Lion Fish Scuba Shop') 
-    end 
+    before { visit root_path } 
     
-    it "shoudl have the base title" do 
-      visit '/static_pages/home'
-      expect(page).to have_title("The Lion Fish Scuba Shop")
-    end 
-    
-    it "should not have a custom page title" do 
-      visit '/static_pages/home'
-      expect(page).to have_title("| Home")
-    end 
+    it { should have_content('The Lion Fish Scuba Shop') } 
+    it { should have_title(full_title('')) } 
+    it { should_not have_title('| Home') } 
   end 
   
   describe "Help page" do 
-    it "should have the content 'Help'" do 
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end 
+    before { visit help_path } 
+    
+    it { should have_content('Help') } 
+    it { should have_title(full_title('Help')) }
   end 
   
-  describe "About" do 
-    it "should have the content 'About Us'" do 
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end 
+  describe "About page" do 
+    before { visit about_path }
+    
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end 
   
 end
