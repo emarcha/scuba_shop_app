939a530c53ccc9275f46f01805d72e084fe0e4f1
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 00:01:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScubaShopApp::Application.config.secret_key_base = '8de36c67d6bb2495cf34bcee44a2271d917ebb6c63b819e7ed3ff6edd2a5d8ef864fa2a4d0f9abc18590897c097aa0891768f6d084c77f997ec9e2b59f4ccd0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScubaShopApp::Application.config.session_store :encrypted_cookie_store, key: '_scuba_shop_app_session'
