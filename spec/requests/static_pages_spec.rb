de5127d59daac3e0d247a081de606bd4a48adb13
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 20:07:52 -0500

    Update test cases

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,17 @@ describe "StaticPages" do
     it_should_behave_like "all static pages" 
   end 
   
+  it "should have teh right links on the layout" do 
+    visit root_path 
+    click_link "About" 
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help" 
+    expect(page).to have_title(full_title('Help'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "The Lion Fish Scuba Shop"
+    expect(page).to have_title(full_title(''))
+  end 
+  
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "The Lion Fish Scuba Shop" 
-  if page_title.empty? 
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end 
-end 
+include ApplicationHelper
