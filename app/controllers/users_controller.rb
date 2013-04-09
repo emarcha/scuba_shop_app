00a890f12a403dedf75930ca188d6ba78ede3a16
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-09 00:44:55 -0500

    Correct typo in success flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "welcome to the Lion Fish Scuba Shop!"
+      flash[:success] = "Welcome to the Lion Fish Scuba Shop!"
       redirect_to @user
     else
       render 'new'
