91a86b85f0a6a2cc7186ca66bd87d888b726d1b3
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 19:49:08 -0500

    Update rspec test cases

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,26 +4,37 @@ describe "StaticPages" do
   
   subject { page } 
   
+  shared_examples_for "all static pages" do 
+    it { should have_content(heading) } 
+    it { should have_title(full_title(page_title)) }
+  end  
+  
   describe "Home page" do 
     before { visit root_path } 
     
-    it { should have_content('The Lion Fish Scuba Shop') } 
-    it { should have_title(full_title('')) } 
+    let(:heading)     { 'The Lion Fish Scuba Shop' } 
+    let(:page_title)  { '' } 
+
+    it_should_behave_like "all static pages"     
     it { should_not have_title('| Home') } 
   end 
   
   describe "Help page" do 
     before { visit help_path } 
     
-    it { should have_content('Help') } 
-    it { should have_title(full_title('Help')) }
+    let(:heading)     { 'Help' } 
+    let(:page_title)  { 'Help' } 
+    
+    it_should_behave_like "all static pages" 
   end 
   
   describe "About page" do 
     before { visit about_path }
     
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading)     { 'About' } 
+    let(:page_title)  { 'About Us' } 
+    
+    it_should_behave_like "all static pages" 
   end 
   
 end
