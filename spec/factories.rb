86b14aa96e1f5008180df71700fd7e6580639632
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 22:32:16 -0500

    Add factories and appropriate tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+  factory :user do 
+    name      "Emmanuel Marcha" 
+    email     "emarcha@example.com"
+    password  "foobar"
+    password_confirmation "foobar" 
+  end 
+end 
