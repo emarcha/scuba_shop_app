396ce004074eb9eea7ea53c7e3c0abd9a9c32599
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 21:53:25 -0500

    Change email downcasing for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  
+  has_secure_password
 
-  before_save { self.email = email.downcase } 
+  before_save { email.downcase! } 
 
   validates :name, 
     presence: true, 
@@ -12,8 +14,6 @@ class User < ActiveRecord::Base
     presence: true, 
     format: { with: VALID_EMAIL_REGEX }, 
     uniqueness: { case_sensitive: false } 
-    
-  has_secure_password
   
   validates :password_confirmation, 
     presence: true
