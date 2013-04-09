4c18b532df985824b0e9089b07d0fb1f79024b6a
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-08 22:02:12 -0500

    Add debug box to dev environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ScubaShopApp::Application.routes.draw do
 
-  get "users/new"
+  resources :users
   root to: 'static_pages#home'
 
-  match '/signup',    to: 'users#new',               via: 'get' 
+  match '/signup',    to: 'users#new',              via: 'get' 
   match '/help',      to: 'static_pages#help',      via: 'get' 
   match '/about',     to: 'static_pages#about',     via: 'get'
   
