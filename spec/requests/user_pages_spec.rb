570b22fe51375415d7f4d3d05933e635ba11d9f7
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-10 04:08:22 -0500

    Add tests and controller updates for editing users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,6 +51,22 @@ describe "UserPages" do
         it { should have_selector('div.alert.alert-success', text: 'Welcome') } 
       end 
     end 
-  end 
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_contact('error') }
+    end
+  end
 
 end
