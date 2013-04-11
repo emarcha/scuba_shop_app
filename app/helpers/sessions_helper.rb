b11a2c193253d609f58ef42c7e3bb70994ab1587
Emmanuel Marcha <info@emmmanuelmarcha.com>
2013-04-10 19:05:30 -0500

    Add correct user for authentication

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,10 @@ module SessionsHelper
   
   def current_user
     @current_user ||= User.find_by(remember_token: cookies[:remember_token])
-  end 
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
 
 end
