16cadaa6a52e9d2d7580850349943f0183217497
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-11 06:55:29 -0500

    Add user deletion for admins

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
     sequence(:name)   { |n| "Example User#{n}"}
     sequence(:email)  { |n| "user#{n}@example.com" }
     password  "foobar"
-    password_confirmation "foobar" 
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end 
 end 
