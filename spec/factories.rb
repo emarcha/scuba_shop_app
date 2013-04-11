c3d3a60ff70122557479d07597cbe2f5ea90fcc4
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-11 04:55:45 -0500

    Add user list view and appropriate tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
   factory :user do 
-    name      "Emmanuel Marcha" 
-    email     "emarcha@example.com"
+    name      "Example User"
+    email     "user@example.com"
     password  "foobar"
     password_confirmation "foobar" 
   end 
