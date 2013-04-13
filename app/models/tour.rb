3415ceac92a3f20fa5633899b74c13fc068d450b
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-12 20:47:39 -0500

    Add test cases for tour model

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,7 @@
 class Tour < ActiveRecord::Base
+
+  validates :title,
+    presence: true,
+    length: { maximum: 100 }
+
 end
--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Tour do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @tour = Tour.new(title: "Test Tour")
+  end
+
+  subject { @tour }
+
+  it { should respond_to(:title) }
+
+  it { should be_valid }
+
+  describe "when title is not present" do
+    before { @tour.title = " "}
+    it { should_not be_valid }
+  end
+
+  describe "when title is too long" do
+    before { @tour.title = "a" * 101}
+    it { should_not be_valid }
+  end
+
 end
