4ef7d9ca70a0087228c27e1251194a9554b30e66
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-14 16:33:51 -0500

    Add semi-complete bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,7 @@
 class BookingsController < ApplicationController
+
+  before_action :signed_in_user
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130414065435) do
+ActiveRecord::Schema.define(version: 20130414191914) do
+
+  create_table "bookings", force: true do |t|
+    t.integer  "tour_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "bookings", ["tour_id", "created_at"], name: "index_bookings_on_tour_id_and_created_at"
 
   create_table "tours", force: true do |t|
     t.string   "title"
