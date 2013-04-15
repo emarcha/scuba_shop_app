c9b0261fb77dc77e40fc0d20b8dbd493aa5ad586
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-15 02:02:55 -0500

    Add initial test cases for tour model

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,8 +2,24 @@ class Tour < ActiveRecord::Base
 
   has_many :bookings
 
+  before_save :populate_available_seats
+
   validates :title,
-    presence: true,
-    length: { maximum: 100 }
+            presence: true,
+            length: { maximum: 100 }
+
+  validates :tour_date,
+            presence: true
+
+  validates :total_seats,
+            presence: true,
+            numericality: { only_integer: true,
+                            greater_than_or_equal_to: 1 }
+
+  private
+
+    def populate_available_seats
+      self.available_seats = self.total_seats
+    end
 
 end
--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -3,25 +3,48 @@ require 'spec_helper'
 describe Tour do
 
   before do
-    @tour = Tour.new(title: "Test Tour",
-                     tour_date: "1/1/2013")
+    @tour = Tour.new(title: 'Test Tour',
+                     tour_date: '1/1/2013',
+                     total_seats: 10)
   end
 
   subject { @tour }
 
   it { should respond_to(:title) }
   it { should respond_to(:tour_date) }
+  it { should respond_to(:total_seats) }
+  it { should respond_to(:available_seats) }
 
   it { should be_valid }
 
-  describe "when title is not present" do
-    before { @tour.title = " "}
+  describe 'when title is not present' do
+    before { @tour.title = ' '}
     it { should_not be_valid }
   end
 
-  describe "when title is too long" do
-    before { @tour.title = "a" * 101}
+  describe 'when title is too long' do
+    before { @tour.title = 'a' * 101}
     it { should_not be_valid }
+  end
+
+  describe 'when date is not present' do
+    before { @tour.tour_date = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'when total seats are not present' do
+    before { @tour.total_seats = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'when total seats are less than 1' do
+    before { @tour.total_seats = 0 }
+    it { should_not be_valid }
+  end
+
+  describe 'available seats should equal total seats upon save' do
+    before { @tour.save }
+    its(:available_seats) { should eql(@tour.total_seats) }
   end
 
 end
