db7f51ebb0c4e49d4acca961a16d6b0f7894aee7
Emmanuel Marcha <info@emmanuelmarcha.com>
2013-04-19 01:16:36 -0500

    Update factories and sample data

--- spec/factories.rb
+++ spec/factories.rb
@@ -20,7 +20,6 @@ FactoryGirl.define do
 
   factory :booking do
     num_seats   2
-    paid        true
     credit_card_number '4539762311332008'
     card_security_code '773'
     card_name   'Card Owner'
